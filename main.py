d4f5043d6e53fce18eec6980902d9645afbf0408
dInOtAvR <103885927+dInOtAvR@users.noreply.github.com>
2022-04-23 10:41:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import 
+from pygame import *
+
+
+back=(200,200,200)
+win_width=600
+win_height=500
+window=display.set_mode((win_width,win_height))
+window.fill(back)
+display.set_caption('pingPong')
+
+game=True
+clock= time.Clock()
+FPS=60
+
+while game:
+    for e in event.get():
+        if e.type== QUIT:
+            game=False
+    display.update()
+    clock.tick(FPS)
