fcc0d6dbaf9309e8a923d712b0df3d9caa4add55
dInOtAvR <103885927+dInOtAvR@users.noreply.github.com>
2022-05-01 15:59:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Player(GameSprite):
         if keys[K_w] and self.rect.y>5:
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y<win_height-130:
-            self.rect.y +=self.speed
+            self.rect.y +=self.speed  
     
 
     def update_r(self):
@@ -31,7 +31,7 @@ class Player(GameSprite):
 
 
 
-back=(100,235,235)
+back=(203,197,111)
 win_width=600
 win_height=500
 window=display.set_mode((win_width,win_height))
@@ -45,7 +45,16 @@ FPS=60
 
 racket1= Player("racket1.png", 25,200,12,120,7)
 racket2= Player("racket2.png",565,200,12,120,7)
-ball= GameSprite("ball.png",200,200,4,50,50)
+ball= GameSprite("joe-biden-40972.png",200,200,50,50,50)
+
+speedx, speedy= 6,6
+score1, score2 = 0,0
+
+font.init()
+font= font.Font(None,35)
+score= font.render('0 : 0', True,(0,0,0))
+lose= font.render('',True,(180,0,0))
+
 
 while game:
     for e in event.get():
@@ -56,9 +65,47 @@ while game:
 
         racket1.update_l()
         racket2.update_r()
+        ball.rect.x  += speedx
+        ball.rect.y += speedy
 
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            speedx *=-1
+
+        if ball.rect.y<0 or ball.rect.y>win_height-50:
+            speedy *= -1
+
+        if ball.rect.x <0:
+            score2 += 1
+            ball.rect.x, ball.rect.y= 200,200
+            speedx, speedy= 6,6
+        
+        if ball.rect.x>win_width-50:
+            score1 += 1
+            ball.rect.x, ball.rect.y= 200,200
+            speedx, speedy= 6,6
+
+        score= font.render(f'{score1} : {score2}',True,(0,0,0))
+        window.blit(score,(285,0))
         racket1.reset()
         racket2.reset()
+        ball.reset()
+
+        if score1   >= 10:
+            lose= font.render('Player 2 lost!',True,(180,0,0))
+            finish=True
+            window.blit(lose,(250,200))
+        if score2   >= 10:
+            lose= font.render('Player 1 lost!',True,(180,0,0))
+            finish=True
+            window.blit(lose,(250,200))
+        
+    else:
+        finish= False
+        time.delay(3000)
+        ball.rect.x,ball.rect.y=200,200
+        speedx,speedy=6,6
+        racket1.rect.y,racket2.rect.y=200,200
+        score1, score2 = 0,0
     
     display.update()
     clock.tick(FPS)
